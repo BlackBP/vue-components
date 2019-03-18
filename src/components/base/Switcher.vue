2d104677fadc7fe312b41887207e9c7547c62d30
BlackBP <blackbp9@gmail.com>
2019-03-19 00:55:59 +1000

    Рефакторинг компонента Switcher.

--- src/components/base/Switcher.vue
+++ src/components/base/Switcher.vue
@@ -1,16 +1,21 @@
 <template>
 
-    <div class="c-switch">
-
-        <input v-model="checked"
-               :type="type"
-               :id="id"
-               :value="val"
-               @change="handleChange">
+    <div class="c-switch"
+         :class="checked ? 'is-active' : ''">
 
         <label ref="label"
                tabindex="0"
+               class="c-switch__label"
                :for="id">
+
+            <input :id="id"
+                   :type="type"
+                   :checked="checked"
+                   :value="value"
+                   :disabled="disabled"
+                   @change="handleChange">
+
+            <span class="c-switch__toggle"></span>
 
             <span class="c-switch__content">
                 <slot></slot>
@@ -25,50 +30,86 @@
 <script>
     export default {
         name: "c-switch",
+        model: {
+            prop: 'model',
+            event: 'change'
+        },
         props: {
             id: {
                 type: String,
                 default: function () {
-                    return this.$uuId('c-radio-')
-                }
+                    return this.$uuId('c-switch-')
+                },
+                required: false
             },
-            checkbox: {
-                type: Boolean,
-                default: true,
+            model: {
+                type: null,
                 required: false
             },
             value: {
                 type: null,
-                default: '',
                 required: false
             },
-            val: {
-                type: null,
-                default: '',
+            name: {
+                type: String,
                 required: false
+            },
+            type: {
+                type: String,
+                default: 'checkbox',
+                required: false,
+                validator(value) {
+                    return ['checkbox', 'radio'].includes(value);
+                }
+            },
+            disabled: {
+                type: Boolean,
+                default: false
             }
         },
-        data() {
-            return {
-                proxy: false
-            };
-        },
         computed: {
-            type() {
-                return this.checkbox ? 'checkbox' : 'radio';
-            },
-            checked: {
-                get() {
-                    return this.value;
-                },
-                set(value) {
-                    this.proxy = value;
+            checked() {
+                if(this.type === 'radio') {
+                    return this.model == this.value;
+                }
+
+                // For checkbox
+                if (this.model instanceof Array) {
+                    return this.model.includes(this.value);
+                } else if (typeof this.model === 'boolean') {
+                    return this.model;
+                } else {
+                    return this.model == this.value;
                 }
             }
         },
         methods: {
-            handleChange(event) {
-                this.$emit('input', this.proxy);
+            handleChange({target: {checked}}) {
+                if(this.type === 'radio') {
+                    this.$emit('change', this.value);
+                    return
+                }
+
+                // For checkbox
+                let isChecked = checked;
+                let value = this.value;
+                let newValue = value;
+
+                if (this.model instanceof Array) {
+                    newValue = [...this.model];
+
+                    if (isChecked) {
+                        newValue.push(value);
+                    } else {
+                        newValue.splice(newValue.indexOf(value), 1);
+                    }
+
+                    this.$emit('change', newValue);
+                } else if (typeof this.model === 'boolean') {
+                    this.$emit('change', isChecked);
+                } else {
+                    this.$emit('change', value);
+                }
             },
             focus() {
                 this.$refs.label.focus();
