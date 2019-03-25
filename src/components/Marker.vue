90d81d604f6ebeaaa67ea957cd0d1fe615f4af6f
BlackBP <blackbp9@gmail.com>
2019-03-25 20:55:39 +1000

    Вёрстка компонента Marker

---
+++ src/components/Marker.vue
@@ -0,0 +1,21 @@
+<template functional>
+    <i v-bind="data.attrs"
+       v-on="listeners"
+       class="c-marker"
+       :style="data.staticStyle"
+       :class="[data.staticClass, {
+            'c-marker--ripple' : props.ripple
+       }]"></i>
+</template>
+
+<script>
+    export default {
+        name: "c-marker",
+        props: {
+            ripple: {
+                type: Boolean,
+                default: false,
+            }
+        }
+    }
+</script>
