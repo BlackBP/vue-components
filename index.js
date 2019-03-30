9e37bd3e6fe1ba735c3caa83d2733b9d0fb2a0ad
BlackBP <blackbp9@gmail.com>
2019-03-30 18:31:26 +1000

    Настройка экспорта.

---
+++ index.js
@@ -0,0 +1,27 @@
+import Btn from './src/components/base/Button'
+import BtnGroup from './src/components/base/ButtonGroup'
+import Checkbox from './src/components/base/Checkbox'
+import Chip from './src/components/base/Chip'
+import Icon from './src/components/base/Icon'
+import IconBtn from './src/components/base/IconButton'
+import Modal from './src/components/base/Modal'
+import Radio from './src/components/base/Radio'
+import Select from './src/components/base/Select'
+import Switcher from './src/components/base/Switcher'
+import TextArea from './src/components/base/TextArea'
+import TextInput from './src/components/base/TextInput'
+
+export {
+    Btn,
+    BtnGroup,
+    Checkbox,
+    Chip,
+    Icon,
+    IconBtn,
+    Modal,
+    Radio,
+    Select,
+    Switcher,
+    TextArea,
+    TextInput
+}
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,22 @@
 module.exports = {
     publicPath: '',
-    outputDir: undefined,
+    outputDir: 'dist',
     assetsDir: undefined,
     runtimeCompiler: undefined,
     productionSourceMap: undefined,
     parallel: undefined,
     css: {
-      modules: true
+        modules: false,
+        loaderOptions: {
+            sass: {
+                data: `@import "@/assets/styles/base/_mixins.scss"; @import "@/assets/styles/base/_variables.scss";`
+            }
+        }
+    },
+    filenameHashing: false,
+    chainWebpack: config => {
+        config.plugins.delete('html');
+        config.plugins.delete('preload');
+        config.plugins.delete('prefetch')
     }
 };
