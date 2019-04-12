4d07419e28b41c3ae856d0e144791757f9343b34
BlackBP <blackbp9@gmail.com>
2019-04-12 16:53:20 +1000

    v0.2.7

---
+++ src/Bundle.vue
@@ -0,0 +1,27 @@
+import Btn from './components/Button'
+import BtnGroup from './components/ButtonGroup'
+import Checkbox from './components/Checkbox'
+import Chip from './components/Chip'
+import Icon from './components/Icon'
+import IconBtn from './components/IconButton'
+import Modal from './components/Modal'
+import Radio from './components/Radio'
+import Select from './components/Select'
+import Switcher from './components/Switcher'
+import TextArea from './components/TextArea'
+import TextInput from './components/TextInput'
+
+export {
+    Btn,
+    BtnGroup,
+    Checkbox,
+    Chip,
+    Icon,
+    IconBtn,
+    Modal,
+    Radio,
+    Select,
+    Switcher,
+    TextArea,
+    TextInput
+}
