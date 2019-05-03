65ee8b30a130270248a04bed2f6195aea2d4e22e
BlackBP <blackbp9@gmail.com>
2019-05-04 01:37:18 +1000

    Исправление компонента List.

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,11 +1,13 @@
-<template>
-    <div class="c-list">
-        <slot></slot>
-    </div>
-</template>
-
 <script>
     export default {
-        name: "c-list"
+        name: "c-list",
+        functional: true,
+        render(createElement, {data, props, children}) {
+            let baseClass = 'c-list';
+
+            data.class = [baseClass, data.class];
+
+            return createElement('div', data, children)
+        }
     }
 </script>
