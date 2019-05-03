d70102142cd0b9cea3d61fb2f52a89e4bf8e6dda
BlackBP <blackbp9@gmail.com>
2019-05-04 01:36:22 +1000

    v0.6.1

---
+++ src/components/StackGroup.vue
@@ -0,0 +1,41 @@
+<script>
+    export default {
+        name: "c-stack-group",
+        functional: true,
+        props: {
+            inline: {
+                type: Boolean,
+                default: false
+            },
+            column: {
+                type: Boolean,
+                default: false
+            },
+            alignItems: {
+                type: String,
+                default: 'center'
+            },
+            alignContent: {
+                type: String,
+                default: 'between',
+            },
+            wrap: {
+                type: Boolean,
+                default: false
+            }
+        },
+        render(createElement, {data, props, children}) {
+            let baseClass = 'c-stack';
+
+            data.class = [data.class, baseClass, {
+                [`${baseClass}--inline`]: props.inline,
+                [`${baseClass}--column`]: props.column,
+                [`${baseClass}--items-${props.alignItems}`]: true,
+                [`${baseClass}--content-${props.alignContent}`]: true,
+                [`${baseClass}--wrap`]: props.wrap,
+            }];
+
+            return createElement('div', data, children);
+        }
+    }
+</script>
---
+++ src/components/StackItem.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        name: "c-stack-item",
+        functional: true,
+        props: {
+            flex: {
+                type: Boolean,
+                default: false
+            }
+        },
+        render(createElement, {data, props, children}) {
+            let baseClass = 'c-stack-item';
+
+            data.class = [baseClass, data.class, {
+                [`${baseClass}--flex`]: props.flex
+            }];
+
+            return createElement('div', data, children)
+        }
+    }
+</script>
