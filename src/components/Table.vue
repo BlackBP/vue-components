4aeeffc9392dbe665aff46ba49085afb8c83255c
BlackBP <blackbp9@gmail.com>
2019-06-12 01:04:37 +1000

    Добавил коспонент Table.

---
+++ src/components/Table.vue
@@ -0,0 +1,90 @@
+<template>
+    <table class="c-table">
+        <colgroup>
+            <slot name="colgroup"/>
+        </colgroup>
+
+        <thead>
+        <tr>
+            <th v-for="(header, key) in headers"
+                :key="`table-header-${key}`">
+                <slot :name="`header-${key}`">
+                    {{ header }}
+                </slot>
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <template v-if="hasData">
+            <tr v-for="(row, rowIndex) in data"
+                :key="`table-row-${rowIndex}`"
+                :class="rowClass"
+                @click="$emit('rowclick', row)">
+
+                <template v-for="(header, headerKey) in headers">
+                    <td v-if="headerKey in row"
+                        :key="`table-col-${rowIndex}-${headerKey}`">
+                        <slot v-bind="{data: row, value: row[headerKey]}"
+                              :name="`col-${headerKey}`">
+                            {{ row[headerKey] }}
+                        </slot>
+                    </td>
+                </template>
+
+            </tr>
+        </template>
+
+        <template v-else>
+            <tr>
+                <td class="u-text-center"
+                    :colspan="columnsCount">
+                    <slot name="is-empty">
+                        Нет данных.
+                    </slot>
+                </td>
+            </tr>
+        </template>
+
+        </tbody>
+
+        <tfoot v-if="$slots['tfoot']">
+        <slot name="tfoot"/>
+        </tfoot>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: "c-table",
+        props: {
+            data: {
+                type: Array,
+                default: () => ([]),
+                required: true
+            },
+            headers: {
+                type: Object,
+                default: () => ({}),
+                required: true
+            },
+            rowClass: {
+                type: String,
+                default: ''
+            }
+        },
+        computed: {
+            hasData() {
+                if (!Array.isArray(this.data)) return false;
+                return this.data.length > 0
+            },
+            columnsCount() {
+                if (typeof this.headers !== 'object') return 0;
+                if (Array.isArray(this.headers)) return 0;
+
+                return Object.keys(this.headers).length;
+            }
+        }
+
+    }
+</script>
--- src/dashboard/views/Home.vue
+++ src/dashboard/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-    <c-card class="u-h100">
+    <c-card>
+        <c-table :headers="tableHeaders"
+                 :data="tableData">
+            <template slot="colgroup">
+                <col width="50px" />
+            </template>
+        </c-table>
     </c-card>
 </template>
 
 <script>
-
     import CCard from "../../components/Card";
-    import CBtn from "../../components/Button";
-    import CLoadingOverlay from "../../components/LoadingOverlay";
+    import CTable from "../../components/Table";
 
     export default {
         name: "view-home",
         components: {
+            CTable,
             CCard
+        },
+        computed: {
+            tableHeaders() {
+                return {
+                    id: '#',
+                    name: 'Name'
+                }
+            },
+            tableData() {
+                return [...Array(300)].map((val, index) => {
+                    return {
+                        id: index + 1,
+                        name: `item ${index + 1}`
+                    }
+                })
+            }
         }
     }
 </script>
