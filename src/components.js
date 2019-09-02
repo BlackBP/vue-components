52075f175271d1f65bcd9bfa0e8ae58f105ba28b
BlackBP <blackbp9@gmail.com>
2019-09-03 00:04:18 +1000

    # Table.vue Изменение вёрстки. Добавил в общий список компонентов.

--- src/components.js
+++ src/components.js
@@ -10,7 +10,7 @@ import CCheckbox from './components/Checkbox.vue';
 import CChip from './components/Chip.vue';
 import CCollapse from './components/Collapse.vue';
 import CContextPopup from './components/ContextPopup.vue';
-import ContextPopupItem from './components/ContextPopupItem.vue';
+import CContextPopupItem from './components/ContextPopupItem.vue';
 import CDivider from './components/Divider.vue';
 import CFormRow from './components/FormRow.vue';
 import CGridCol from './components/GridCol.vue';
@@ -31,6 +31,7 @@ import CServiceFormValidate from './components/ServiceFormValidate.vue';
 import CStackGroup from './components/StackGroup.vue';
 import CStackItem from './components/StackItem.vue';
 import CSwitch from './components/Switch.vue';
+import CTable from './components/Table.vue';
 import CTextArea from './components/TextArea.vue';
 import CTextInput from './components/TextInput.vue';
 
@@ -43,7 +44,7 @@ const Components = {
     CChip,
     CCollapse,
     CContextPopup,
-    ContextPopupItem,
+    CContextPopupItem,
     CDivider,
     CFormRow,
     CGridCol,
@@ -64,6 +65,7 @@ const Components = {
     CStackGroup,
     CStackItem,
     CSwitch,
+    CTable,
     CTextArea,
     CTextInput
 };
@@ -77,7 +79,7 @@ export {
     CChip,
     CCollapse,
     CContextPopup,
-    ContextPopupItem,
+    CContextPopupItem,
     CDivider,
     CFormRow,
     CGridCol,
@@ -98,6 +100,7 @@ export {
     CStackGroup,
     CStackItem,
     CSwitch,
+    CTable,
     CTextArea,
     CTextInput
 }
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,58 +1,60 @@
 <template>
-    <table class="c-table">
-        <colgroup>
-            <slot name="colgroup"/>
-        </colgroup>
+    <div class="c-table-wrap">
+        <table class="c-table">
+            <colgroup>
+                <slot name="colgroup"/>
+            </colgroup>
 
-        <thead>
-        <tr>
-            <th v-for="(header, key) in headers"
-                :key="`table-header-${key}`">
-                <slot :name="`header-${key}`">
-                    {{ header }}
-                </slot>
-            </th>
-        </tr>
-        </thead>
-        <tbody>
+            <thead>
+            <tr>
+                <th v-for="(header, key) in headers"
+                    :key="`table-header-${key}`">
+                    <slot :name="`header-${key}`">
+                        {{ header }}
+                    </slot>
+                </th>
+            </tr>
+            </thead>
+            <tbody>
 
-        <template v-if="hasData">
-            <tr v-for="(row, rowIndex) in data"
-                :key="`table-row-${rowIndex}`"
-                :class="rowClass"
-                @click="$emit('row-click', row, rowIndex)"
-                @contextmenu="$emit('row-contextmenu', $event, row, rowIndex)">
+            <template v-if="hasData">
+                <tr v-for="(row, rowIndex) in data"
+                    :key="`table-row-${rowIndex}`"
+                    :class="rowClass"
+                    @click="$emit('row-click', row, rowIndex)"
+                    @contextmenu="$emit('row-contextmenu', $event, row, rowIndex)">
 
-                <template v-for="(header, headerKey) in headers">
-                    <td v-if="headerKey in row"
-                        :key="`table-col-${rowIndex}-${headerKey}`">
-                        <slot v-bind="{rowData: row, rowIndex: rowIndex, value: row[headerKey]}"
-                              :name="`col-${headerKey}`">
-                            {{ row[headerKey] }}
+                    <template v-for="(header, headerKey) in headers">
+                        <td v-if="headerKey in row"
+                            :key="`table-col-${rowIndex}-${headerKey}`">
+                            <slot v-bind="{rowData: row, rowIndex: rowIndex, value: row[headerKey]}"
+                                  :name="`col-${headerKey}`">
+                                {{ row[headerKey] }}
+                            </slot>
+                        </td>
+                    </template>
+
+                </tr>
+            </template>
+
+            <template v-else>
+                <tr>
+                    <td class="u-text-center"
+                        :colspan="columnsCount">
+                        <slot name="is-empty">
+                            Нет данных.
                         </slot>
                     </td>
-                </template>
+                </tr>
+            </template>
 
-            </tr>
-        </template>
+            </tbody>
 
-        <template v-else>
-            <tr>
-                <td class="u-text-center"
-                    :colspan="columnsCount">
-                    <slot name="is-empty">
-                        Нет данных.
-                    </slot>
-                </td>
-            </tr>
-        </template>
-
-        </tbody>
-
-        <tfoot v-if="$slots['tfoot']">
-        <slot name="tfoot"/>
-        </tfoot>
-    </table>
+            <tfoot v-if="$slots['tfoot']">
+            <slot name="tfoot"/>
+            </tfoot>
+        </table>
+    </div>
 </template>
 
 <script>
