2055c7eb6ad29e576613f3a8692fd64cfdb96e37
blackbp <blackbp9@gmail.com>
2019-11-16 21:20:32 +1000

    Вёрстка docs для GitHub Pages.

--- docs/src/plugins/components.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import Components from '../../../dist/lib.esm';
-
-Vue.use(Components);
---
+++ src-docs/plugins/components.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import Components from '@blackbp/vue-components';
+
+Vue.use(Components);
