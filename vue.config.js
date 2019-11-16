6d61fb06f79d90cb25ec00d3cefdf31f1a069fcb
blackbp <blackbp9@gmail.com>
2019-11-16 21:35:08 +1000

    Вёрстка docs для GitHub Pages.

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
     productionSourceMap: false,
     publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
     css: {
-        extract: false
+        extract: true
     }
 };
