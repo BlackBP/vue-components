469919e4128a3bd82d1343b9ac24203794ed908e
blackbp <blackbp9@gmail.com>
2019-12-04 23:53:32 +1000

    Тест компонента, использующий popper.js.

--- src-docs/views/Home.vue
+++ src-docs/views/Home.vue
@@ -2,16 +2,33 @@
     <layout-screen-card header="Components grid"
                         icon="view-dashboard">
 
+        <c-btn id="button"
+               @click="visible = !visible">
+            Show popper
+        </c-btn>
+
+        <c-popper reference="#button"
+                  style="width: auto;"
+                  :visible="visible">
+            Hello world!
+        </c-popper>
     </layout-screen-card>
 </template>
 
 <script>
     import LayoutScreenCard from "./layouts/ScreenCard.vue";
+    import CPopper from "../components/test/popper";
 
     export default {
         name: "ScreenHome",
         components: {
+            CPopper,
             LayoutScreenCard
+        },
+        data() {
+            return {
+                visible: false
+            }
         },
     }
 </script>
